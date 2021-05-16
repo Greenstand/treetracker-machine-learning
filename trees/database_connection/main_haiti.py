7e1be010697c8f90352e89b49da3c641d067fc79
daint90 <pizdaint90@gmail.com>
2021-05-16 19:21:43 +0000

    create multiple tasks for a given number of target images per task

--- trees/database_connection/main_haiti.py
+++ trees/database_connection/main_haiti.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python
 
 import os
+import sys
 import argparse
 import db_connection
-import cvat_task_manager
 from config import config
+from datetime import date
+
+
+cvat_base_dir = os.path.abspath('./cvat_greenstand')  # should point to the installation of cvat
+sys.path.insert(1, os.path.join(cvat_base_dir, 'cvat/utils/cli'))
+
+import cvat_task_manager
 
 """
 This script requires to have a separate file called "database.ini" with credentials to create the ssh tunnel, 
@@ -62,14 +69,16 @@ def create_new_cvat_task_list(results):
     return img_urls
 
 
-default_labels = ["HAS_TREE", "NO_TREE"]
+default_labels = ["HAS_TREE", "NO_TREE", "SOIL", "UNKNOWN"]
 
 def get_haiti_species():
 
-    haiti_species = ["UNKNOWN", "ACACAURI", "ANACOCCI", "ANNOMURI", "ARTOALTA", \
-                     "CATALONG", "CEDRODOR", "CITR0000", "COCONUCI", "INDE0002", \
-                     "INGAFEUI", "MANGINDI", "MANIZAPO", "MORIOLEI", "PERSAMER", \
-                     "PSIDGUAJ", "TAMAINDI", "TERMCATA", "THEOCACA"]
+    haiti_species = ["ACACAURI", "ANACOCCI", "ANNOMURI", "ANNOSQUA", \
+                     "ARTOALTA", "CATALONG", "CEDRODOR", "CITR0000", \
+                     "COCONUCI", "COLUARBO", "CORDALLI", "INDE0002", \
+                     "INDE0003", "INGAFEUI",  "MANGINDI", "MANIZAPO", \
+                     "MORIOLEI", "PERSAMER", "PSIDGUAJ", "SIMAGLAU", \
+                     "TAMAINDI", "TERMCATA", "THEOCACA"]
 
     return default_labels + haiti_species
 
@@ -87,7 +96,7 @@ def create_json_labels(working_folder, species):
     with open(json_labels_file_name, "w") as f:
         json.dump(species_list, f, indent=4)
 
-    return json_labels_file_name
+    return (json_labels_file_name, species_list)
 
 
 if __name__ == '__main__':
@@ -97,14 +106,15 @@ if __name__ == '__main__':
     parser.add_argument('--restart-logging', type=int, default=0, help='boolean to delete previous logging and start pulling image urls from scratch')
     parser.add_argument('--test-images', type=str, default="all", help='Number of images to test with, "all" for all images')
     parser.add_argument('--cvat-cli', type=str, default='./cli.py', help='Path to cli.py cvat script')
-    parser.add_argument('--create-cvat-task', type=int, default=0, help='boolean: create cvat task or not')
+    parser.add_argument('--images-per-cvat-task', type=int, default=0, help='int: number of images per cvat task.')
     parser.add_argument('--working-folder', type=str, default='./', help='path to folder where databases.ini is and for saving new files.')
+    parser.add_argument('--prefix', type=str, default='', help='prefix to be appended.')
     parsed_args = parser.parse_args()
 
     working_folder = parsed_args.working_folder
 
-    log_file_name = working_folder + "log_queries.txt"
-    img_urls_file_name = working_folder + "img_urls.txt"
+    log_file_name = os.path.join(working_folder, "log_queries.txt")
+    img_urls_file_name = os.path.join(working_folder, parsed_args.prefix + "img_urls.txt")
     haiti_labels_file_name = os.path.join(working_folder, "haiti_labels.json")
 
     if parsed_args.restart_logging:
@@ -128,7 +138,7 @@ if __name__ == '__main__':
         with open(log_file_name, "a") as f:
             f.write(last_timestamp + "\n")
 
-    credentials_file = working_folder + 'database.ini'
+    credentials_file = os.path.join(working_folder, 'database.ini')
     tunnel = db_connection.create_ssh_tunnel(credentials_file)
     tunnel.start()
     (connection, cur) = db_connection.connect(credentials_file)
@@ -136,35 +146,48 @@ if __name__ == '__main__':
     haiti_trees_query = get_haiti_trees_query(last_timestamp)
     results = db_connection.execute_query(cur, haiti_trees_query)
 
+    db_connection.close_connection(connection, cur)
+    tunnel.close()
+
+    images_per_task = parsed_args.images_per_cvat_task
+
     if len(results) > 0:
 
         test_images = parsed_args.test_images
 
         if test_images != "all":
             test_images = int(test_images)
+        else:
+            test_images = len(results)
+
+        if images_per_task > 0:
+            # Remove images which do not fit new task.
+            # They'll be used for next task creation.
+            test_images = test_images - (test_images % images_per_task)
             results = results[:test_images]
 
-        images_urls = create_new_cvat_task_list(results)
-        update_query_log(log_file_name, results)
+            numOfNewTasks = int(test_images / images_per_task)
 
-        with open(img_urls_file_name, "a") as f:
+            images_urls = create_new_cvat_task_list(results)
+            update_query_log(log_file_name, results)
 
-            for this_url in images_urls:
-                f.write(this_url + "\n")
+            with open(img_urls_file_name, "a") as f:
+                for this_url in images_urls:
+                    f.write(this_url + "\n")
+
+            cvat_params = config(credentials_file, 'cvat_local')
+
+            cvat_mgr = cvat_task_manager.CvatManager(parsed_args.cvat_cli, cvat_params)
+            cvat_mgr.get_current_cvat_tasks()
+
+            if len(images_urls) > 0:
+                haiti_species = get_haiti_species()
+                (labels_json_file, labels) = create_json_labels(working_folder, haiti_species)
+                task_prefix = parsed_args.prefix + "haiti_" + str(date.today()) + "_" + "_task_"
+
+                for i in range(numOfNewTasks):
+                    task_name = task_prefix + str(i)
+                    cvat_mgr.create_new_cvat_task(images_urls[i*images_per_task:(i+1)*images_per_task ], labels_json_file, task_name)
+
     else:
         print("No images left in the db.")
-
-    db_connection.close_connection(connection, cur)
-    tunnel.close()
-
-    cvat_params = config(credentials_file, 'cvat_local')
-
-    cvat_mgr = cvat_task_manager.CvatManager(parsed_args.cvat_cli, cvat_params)
-    cvat_mgr.get_current_cvat_tasks()
-
-    if parsed_args.create_cvat_task and len(images_urls) > 0:
-
-        haiti_species = get_haiti_species()
-        json_labels_file_name = create_json_labels(working_folder, haiti_species)
-        cvat_mgr.create_new_cvat_task(images_urls, json_labels_file_name, "haiti_annotations_")
-
