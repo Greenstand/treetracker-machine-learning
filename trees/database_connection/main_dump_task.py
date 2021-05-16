51a2d877581c989c53b763045364be02b8650327
daint90 <pizdaint90@gmail.com>
2021-05-16 19:39:11 +0000

    dump multiple tasks

--- trees/database_connection/main_dump_task.py
+++ trees/database_connection/main_dump_task.py
@@ -1,8 +1,12 @@
 
-import cvat_task_manager
 import argparse
 from config import config
-import os
+import os, sys
+
+cvat_base_dir = os.path.abspath('./cvat_greenstand')  # should point to the installation of cvat
+sys.path.insert(1, os.path.join(cvat_base_dir, 'cvat/utils/cli'))
+
+import cvat_task_manager
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -10,18 +14,21 @@ if __name__ == '__main__':
     parser.add_argument('--cvat-cli', type=str, default='./cli.py', help='Path to cli.py cvat script')
     parser.add_argument('--working-folder', type=str, default='./',
                         help='path to folder where databases.ini is and for saving new files.')
+    parser.add_argument('--target-folder', type=str, default='./',
+                        help='path to folder where to dump annotations.')
 
     parsed_args = parser.parse_args()
 
     working_folder = parsed_args.working_folder
+    target_folder = parsed_args.target_folder
 
-    credentials_file = working_folder + 'database.ini'
+    credentials_file = os.path.join(working_folder, 'database.ini')
     cvat_params = config(credentials_file, 'cvat_local')
 
-    project_dir = os.path.abspath('../../../../')
-    target_folder = os.path.join(project_dir, "greenstand/sample_images/dumped_annotations")
+    project_dir = os.path.abspath('.')
 
-    cvat_mgr = cvat_task_manager.CvatManager(os.path.join(project_dir, "cvat/utils/cli/cli.py"), cvat_params)
+    cvat_mgr = cvat_task_manager.CvatManager(parsed_args.cvat_cli, cvat_params)
     completed_tasks = cvat_mgr.get_current_cvat_tasks(status='completed')
 
-    cvat_mgr.dump_target_annotations(completed_tasks[0]['id'], os.path.join(target_folder, "output_" + str(completed_tasks[0]['id']) + ".zip"))
+    for done_task in completed_tasks:
+        cvat_mgr.dump_target_annotations(done_task['id'], os.path.join(target_folder, "output_" + str(done_task['id']) + ".zip"))
